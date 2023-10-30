5fce3d3eb9723412b3ff0861267755e171e41861
Ysmael Requena <ysmaelrequena@gmail.com>
2023-10-30 14:17:00 -0400

    Modified how the program works, takes inputs and returns the data from the character the user requests

--- web_scraper.py
+++ web_scraper.py
@@ -1,44 +1,113 @@
 from bs4 import BeautifulSoup
 import requests
-import textwrap
+import asyncio
 
-character_request = print(f"Which character's data do you want to see?>")
-character_input = input('>')
-html_text = requests.get('https://wiki.supercombo.gg/w/Street_Fighter_6/Chun-Li').text
-soup = BeautifulSoup(html_text, 'lxml')
-move_names = soup.find_all('div', attrs= {'class': 'movedata-flex-framedata-name'})
-table = soup.find_all('table', class_='wikitable citizen-table-nowrap')
+characters = {
+    'aki': 'A.K.I',
+    'blanka': 'Blanka',
+    'cammy': 'Cammy',
+    'chunli': 'Chun-Li',
+    'deejay': 'Dee_Jay',
+    'dhalsim': 'Dhalsim',
+    'honda': 'E.Honda',
+    'guile': 'Guile',
+    'jamie': 'Jamie',
+    'jp': 'JP',
+    'juri': 'Juri',
+    'ken': 'Ken',
+    'kimberly': 'Kimberly',
+    'lily': 'Lily',
+    'luke': 'Luke',
+    'manon': 'Manon',
+    'marisa': 'Marisa',
+    'rashid': 'Rashid',
+    'ryu': 'Ryu',
+    'zangief': 'Zangief'
+}
 
-#Lo siguiente es modificar la funcion para que segun el personaje me de la data. Es decir, hay que volverlo un metodo
+html_text = ''
+soup = ''
+move_names = ''
+table = ''
+move_nom_data = []
+move_name_data = []
+character_movelist = {}
+character_framedata = []
 
-def find_move_names(move_names):
-    if move_names:
-        name_data = []
-        for move in move_names:
-            divs = move.find('div')
-            names = [div.get_text(strip=True) for div in divs]
-            name_data.extend(names)
-        print(name_data[0])
-        
-        if name_data:
-            return [name_data[0]]
-        else: 
-            return None
+
+
+async def character_scrape():
+    character_request = print(f"Which character's data do you want to see?>")
+    character_input = input().lower()
+    character = characters.get(character_input)
+
+    if character:
+        print(character)
+    elif character_input in ['a.k.i.', 'a k i']:
+        character = characters['aki']
+        print(character)
+    elif character_input in ['chun', 'chun-li', 'chun li']:
+        character = characters['chunli']
+        print(character)
+    elif character_input in ['dee jay', 'dee_jay']:
+        character = characters['deejay']
+        print(character)
+    elif character_input in ['e.honda', 'ehonda', 'e honda']:
+        character = characters['honda']
+        print(character)
+    elif character_input in ['j.p', 'j.p.']:
+        character = characters['jp']
+        print(character)
     else:
-        print('Move not found')
+        print('Character not found, please try again')
+    
+    if character: 
+        html_text = await fetch_character_data(character)
 
-find_move_names(move_names[0])
+        if html_text:
+            soup = BeautifulSoup(html_text, 'lxml')
+            move_names = soup.find_all('div', class_='movedata-flex-framedata-name')
+            table = soup.find_all('table', attrs={'class':'wikitable citizen-table-nowrap'})
+
+#Organizamoslos nombres de cada movimientos organizados junto a su respecitva nomeclatura
+            if move_names:
+               
+                for move in move_names:
+                    nom_divs = move.find('div')
+                    name_divs = move.find('div', attrs={'style':"font-size:80%"})
+                    moves_full_name = [name_divs.get_text(strip=True)]
+                    nomenclature = [div.get_text(strip=True) for div in nom_divs]
+                    move_nom_data.extend(nomenclature)
+                    move_name_data.extend(moves_full_name)
+                for key, value in zip(move_nom_data, move_name_data):
+                         character_movelist[key] = value
+                         
+                print(f'The character has {len(move_nom_data)} moves:')
+                
+#Recolectar y organizar framedata de cada movimiento
+            if table:
+                for row in table.find('tr'):
+                    row_data = [td.get_text(strip=True) for td in row.find_all('td')]
+                print(row_data)
+            else:
+                print('Table not found')
 
 
-def find_framedata(table):
-    if table:
-        for row in table.find_all('tr'):
-            row_data = [td.get_text(strip=True) for td in row.find_all('td')]
-        print(row_data)
-        return row_data
+
+async def fetch_character_data(character_name):
+    url = f'https://wiki.supercombo.gg/w/Street_Fighter_6/{character_name}'
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.text
     else:
-        print('Table not found')
-find_framedata(table[0])
+        print(f"Failed to fetch data for {character_name}")
+        return None
+
+asyncio.run(character_scrape())
+
+
+    
+
 
 
 
